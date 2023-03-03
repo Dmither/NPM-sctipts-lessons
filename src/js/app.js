6dc369385cbbe1443af0b46efc0f525e6033cee8
Dmither <mr.d.graumann@gmail.com>
2023-03-03 17:20:34 +0200

    update

---
+++ src/js/app.js
@@ -0,0 +1,4 @@
+import { module } from "./module";
+let x = 5, y = 8;
+let input = document.querySelector('input');
+console.log(module.field1);
---
+++ src/js/module.js
@@ -0,0 +1,4 @@
+export let module = {
+  "field1": "val1",
+  "field2": "val2"
+}
